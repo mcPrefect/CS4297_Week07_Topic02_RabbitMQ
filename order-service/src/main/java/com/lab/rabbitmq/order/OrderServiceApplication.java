9bc629ef4784ffdfaabb4e1bfeb527088ae55479
DavidThorne22365443 <22365443@studentmail.ul.ie>
2025-10-21 12:03:51 +0100

    david commit for section 3 of lab sheet

--- order-service/src/main/java/com/lab/rabbitmq/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/lab/rabbitmq/order/OrderServiceApplication.java
@@ -1,13 +1,24 @@
 package com.lab.rabbitmq.order;
 
+import com.lab.rabbitmq.order.publisher.PaymentPublisher;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
+	@Autowired
+	private PaymentPublisher paymentPublisher; // injects your PaymentPublisher bean
+
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void sendTestMessage() {
+		// runs automatically after app starts
+		paymentPublisher.sendPaymentRequest("Order12345");
+	}
 }
